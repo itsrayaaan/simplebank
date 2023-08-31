acff4d2d7cd286c9dff0fecfb9d87c177a4d102d
Mohammed Rayan <itsrayaaan@gmail.com>
2023-08-31 20:38:53 +0530

    initialized viper for env files

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,19 +7,19 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:defcon5@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/techschool/simplebank/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Cannot connect to the database: ", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("Cannot connect to the database: ", err)
