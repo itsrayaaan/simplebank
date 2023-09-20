d041f94cc0ed75cdbe250063a69a5f498238b218
Mohammed Rayan <itsrayaaan@gmail.com>
2023-09-21 00:44:04 +0530

    login api setup

--- main.go
+++ main.go
@@ -24,7 +24,11 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
-	server := api.NewServer(store)
+	server, err := api.NewServer(config, store)
+
+	if err != nil {
+		log.Fatal("Cannot Create Server", err)
+	}
 
 	err = server.Start(config.ServerAddress)
 
